d417ad4a9cfbc8dd48428b38c70fb86409cb1c37
kef7 <kef7.dev@gmail.com>
2023-05-12 14:20:20 -0400

    build out connectionless strategy with strategy base class

--- NetworkListener/ConnectionlessServerStrategy.cs
+++ NetworkListener/ConnectionlessServerStrategy.cs
@@ -4,56 +4,270 @@
     using NetworkListenerCore.NetworkClientDataProcessors;
     using System.Net;
     using System.Net.Sockets;
+    using System.Xml.Linq;
 
     /// <summary>
     /// Server strategy for a connection-less based network client processing; like UDP clients.
     /// </summary>
-    internal class ConnectionlessServerStrategy : IServerStrategy
+    internal class ConnectionlessServerStrategy : ServerStrategy
     {
-        protected ILogger Logger { get; }
-
-        /// <summary>
-        /// Client connected event signature
-        /// </summary>
-        public event EventHandler<ClientConnectedEventArgs>? ClientConnected = null;
-
-        /// <summary>
-        /// Client data received event signature
-        /// </summary>
-        public event EventHandler<ClientDataReceivedEventArgs>? ClientDataReceived = null;
-
-        /// <summary>
-        /// Client disconnected event signature
-        /// </summary>
-        public event EventHandler<ClientDisconnectedEventArgs>? ClientDisconnected = null;
-
-        /// <summary>
-        /// Client error event signature
-        /// </summary>
-        public event EventHandler<ClientErrorEventArgs>? ClientError = null;
-
         /// <summary>
         /// CTOR
         /// </summary>
         /// <param name="logger">Generic logger</param>
         public ConnectionlessServerStrategy(ILogger logger)
+            : base(logger)
         {
-            Logger = logger;
         }
 
-        public Socket InitServer(IPEndPoint ipEndPoint, SocketType type, ProtocolType protocolType)
+        /// <inheritdoc cref="IServerStrategy.InitServer(IPEndPoint, SocketType, ProtocolType)"/>
+        public override Socket InitServer(IPEndPoint ipEndPoint, SocketType type, ProtocolType protocolType)
         {
-            return null!;
+            // New up server socket
+            var serverSocket = new Socket(ipEndPoint.AddressFamily, type, protocolType);
+
+            // Configure server socket
+            serverSocket.Bind(ipEndPoint);
+
+            return serverSocket;
         }
 
-        public async Task<ClientThreadMeta> RunClientThread(Socket serverSocket, Func<INetworkClientDataProcessor> networkClientDataProcessorFactory, CancellationToken cancellationToken)
+        /// <inheritdoc cref="IServerStrategy.RunClientThread(Socket, Func{INetworkClientDataProcessor}, CancellationToken)"/>
+        public override async Task<ClientThreadMeta> RunClientThread(Socket serverSocket, Func<INetworkClientDataProcessor> networkClientDataProcessorFactory, CancellationToken cancellationToken)
         {
-            await Task.Run(() => { }, cancellationToken);
+            await base.RunClientThread(serverSocket, networkClientDataProcessorFactory, cancellationToken);
+
+            Logger.LogInformation("Waiting to receive on {EndPoint}", serverSocket.LocalEndPoint);
+
+            // Define a client endpoint for UDP comms sent from any source; will be assigned value later
+            var remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            var remoteEndPoint = remoteIpEndPoint as EndPoint;
+
+            try
+            {
+                // Check for cancellation
+                if (CancellationToken.IsCancellationRequested)
+                {
+                    return ClientThreadMeta.None;
+                }
+
+                // Generate client data processor
+                var clientDataProcessor = ClientDataProcessorFactory.Invoke();
+
+                // Build buffer
+                var buffer = new byte[clientDataProcessor.MaxBufferSize];
+
+                // Receive bytes of data
+                var networkResults = await serverSocket.ReceiveFromAsync(buffer, SocketFlags.None, remoteEndPoint, CancellationToken);
+
+                // Check for cancellation
+                if (CancellationToken.IsCancellationRequested)
+                {
+                    return ClientThreadMeta.None;
+                }
+
+                // Cast remote client end-point
+                remoteIpEndPoint = networkResults.RemoteEndPoint as IPEndPoint;
+
+                // Client cancellation token source
+                var cts = new CancellationTokenSource();
+
+                // Create new client thread to handle further procssing
+                var thread = new Thread(() =>
+                {
+                    // Process the client connection and wait.
+                    var triggeredOnClientDisconnected = false;
+                    try
+                    {
+                        // Wait for client processing
+                        var task = new Task(async () =>
+                        {
+                            // Init client data processor
+                            clientDataProcessor.Initialize(remoteIpEndPoint!);
+
+                            // Check for cancellation
+                            if (CancellationToken.IsCancellationRequested)
+                            {
+                                Logger.LogWarning("Client [{ClientRemoteEndPoint}] - Cancellation requested for client", remoteIpEndPoint);
+                                return;
+                            }
+
+                            // Send bytes to client data processor
+                            try
+                            {
+                                clientDataProcessor.ReceiveBytes(buffer, networkResults.ReceivedBytes, 1);
+                            }
+                            catch (Exception ex)
+                            {
+                                var errMsg = "ERR-NCDP-03: Error in client data processor received bytes call";
+                                Logger.LogError(ex, errMsg);
+
+                                // Trigger client error event
+                                OnClientError(remoteIpEndPoint, new AggregateException(errMsg, ex));
+                            }
+
+                            // Check for cancellation
+                            if (CancellationToken.IsCancellationRequested)
+                            {
+                                Logger.LogWarning("Client [{ClientRemoteEndPoint}] - Cancellation requested for client", remoteIpEndPoint);
+                                return;
+                            }
+
+                            // Get client data and allow processor to process it
+                            try
+                            {
+                                // Get data
+                                var data = clientDataProcessor.GetData();
+
+                                // Process received data
+                                clientDataProcessor.ProcessData(data);
+
+                                // Trigger data received event if needed
+                                OnClientDataReceived(remoteEndPoint, data);
+                            }
+                            catch (Exception ex)
+                            {
+                                var errMsg = "ERR-NCDP-04: Error processing client data";
+                                Logger.LogError(ex, errMsg);
+
+                                // Trigger client error event
+                                OnClientError(remoteIpEndPoint, new AggregateException(errMsg, ex));
+                            }
+
+                            // Check cancellation
+                            if (cancellationToken.IsCancellationRequested)
+                            {
+                                Logger.LogWarning("Client [{ClientRemoteEndPoint}] - Cancellation requested for client", remoteIpEndPoint);
+                                return;
+                            }
+
+                            // Get byte data to send to client from client data processor
+                            var sendIteration = 1;
+                            try
+                            {
+                                // Iterate over client data send processing
+                                var continueToSend = false;
+                                var totalSent = 0;
+                                do
+                                {
+                                    // Check cancellation
+                                    if (cancellationToken.IsCancellationRequested)
+                                    {
+                                        Logger.LogWarning("Client [{ClientRemoteEndPoint}] - Cancellation requested for client", remoteIpEndPoint);
+                                        break;
+                                    }
+
+                                    // Declare buffer
+                                    buffer = null;
+
+                                    // Get send bytes from client data processor
+                                    continueToSend = clientDataProcessor.SendBytes(out buffer, totalSent, sendIteration);
+
+                                    // Send bytes to client if needed
+                                    if (buffer?.Length > 0)
+                                    {
+                                        Logger.LogInformation("Client [{ClientRemoteEndPoint}] - Sending data on iteration [{Iteration}]", remoteIpEndPoint, sendIteration);
+
+                                        // Send acknowledgment to client
+                                        var sendResults = await serverSocket.SendToAsync(buffer, SocketFlags.None, remoteIpEndPoint!);
+
+                                        Logger.LogDebug("Client [{ClientRemoteEndPoint}] - Sending [{BytesSent}] bytes on iteration [{Iteration}]", remoteIpEndPoint, buffer.Length, sendIteration);
+
+                                        // Increment total sent
+                                        totalSent += buffer.Length;
+                                    }
+
+                                    // Increment iteration
+                                    sendIteration += 1;
+                                }
+                                while (continueToSend);
+
+                                Logger.LogDebug("Client [{ClientRemoteEndPoint}] - Sent [{BytesSent}] in total", remoteIpEndPoint, totalSent);
+
+                                // Check cancellation
+                                if (cancellationToken.IsCancellationRequested)
+                                {
+                                    Logger.LogWarning("Client [{ClientRemoteEndPoint}] - Cancellation requested for client", remoteIpEndPoint);
+                                    return;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                var errMsg = "ERR-NCDP-05: Error retrieving client data processor send bytes";
+                                Logger.LogError(ex, errMsg);
+
+                                // Trigger client error event
+                                OnClientError(remoteIpEndPoint, new AggregateException(errMsg, ex));
+                            }
+                        });
+                        Task.WaitAll(new Task[] { task }, CancellationToken);
+                    }
+                    catch (AggregateException aggEx)
+                    {
+                        // Get base exception
+                        var ex = aggEx.GetBaseException();
+
+                        // Trigger client error event
+                        OnClientError(remoteIpEndPoint, ex);
+
+                        // Check if canceled
+                        if (ex is OperationCanceledException)
+                        {
+                            Logger.LogWarning("Client [{ClientRemoteEndPoint}] - Processing thread canceled", remoteIpEndPoint);
+
+                            // Trigger client disconnected event
+                            OnClientDisconnected(remoteIpEndPoint, ex as OperationCanceledException);
+                            triggeredOnClientDisconnected = true;
+                        }
+                        // Check if disconnected abruptly
+                        else if (ex is SocketException sEx && sEx.NativeErrorCode == 10054)
+                        {
+                            Logger.LogError("Client [{ClientRemoteEndPoint}] - Disconnected abruptly", remoteIpEndPoint);
+
+                            // Trigger client disconnected event
+                            OnClientDisconnected(remoteIpEndPoint, sEx);
+                            triggeredOnClientDisconnected = true;
+                        }
+                        else
+                        {
+                            Logger.LogError(ex, "Client [{ClientRemoteEndPoint}] - Error in client processing", remoteIpEndPoint);
+                        }
+                    }
+                    finally
+                    {
+                        // Should we trigger client disconnected event
+                        if (!triggeredOnClientDisconnected)
+                        {
+                            // Trigger client disconnected event
+                            OnClientDisconnected(remoteIpEndPoint);
+                        }
+                    }
+                });
+
+                // Configure thread
+                thread.IsBackground = true;
+                thread.Name = Guid.NewGuid().ToString();
+                thread.Priority = ThreadPriority.Normal;
+
+                // Build client thread meta
+                var ctMeta = new ClientThreadMeta
+                {
+                    Name = thread.Name!,
+                    Thread = thread,
+                    CancellationTokenSource = cts
+                };
+
+                // Start thread
+                thread.Start();
+
+                return ctMeta;
+            }
+            catch (Exception ex)
+            {
+                OnClientError(null, ex);
+            }
+
             return ClientThreadMeta.None;
-        }
-
-        public void Dispose()
-        {
         }
     }
 }
---
+++ NetworkListener/ServerStrategy.cs
@@ -0,0 +1,127 @@
+﻿namespace NetworkListenerCore
+{
+    using Microsoft.Extensions.Logging;
+    using NetworkListenerCore;
+    using NetworkListenerCore.NetworkClientDataProcessors;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal abstract class ServerStrategy : IServerStrategy
+    {
+        /// <summary>
+        /// Logger
+        /// </summary>
+        protected virtual ILogger Logger { get; }
+
+        /// <summary>
+        /// Cancellation token
+        /// </summary>
+        protected CancellationToken CancellationToken { get; set; } = CancellationToken.None;
+
+        /// <summary>
+        /// The network client data processor factory that produces client data processors for each client connection
+        /// </summary>
+        public Func<INetworkClientDataProcessor> ClientDataProcessorFactory { get; internal set; } = null!;
+
+        /// <inheritdoc cref="IClientEvents.ClientConnected"/>
+        public event EventHandler<ClientConnectedEventArgs>? ClientConnected = null;
+
+        /// <inheritdoc cref="IClientEvents.ClientDataReceived"/>
+        public event EventHandler<ClientDataReceivedEventArgs>? ClientDataReceived = null;
+
+        /// <inheritdoc cref="IClientEvents.ClientDisconnected"/>
+        public event EventHandler<ClientDisconnectedEventArgs>? ClientDisconnected = null;
+
+        /// <inheritdoc cref="IClientEvents.ClientError"/>
+        public event EventHandler<ClientErrorEventArgs>? ClientError = null;
+
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        public ServerStrategy(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public abstract Socket InitServer(IPEndPoint ipEndPoint, SocketType type, ProtocolType protocolType);
+
+        public virtual async Task<ClientThreadMeta> RunClientThread(Socket serverSocket, Func<INetworkClientDataProcessor> networkClientDataProcessorFactory, CancellationToken cancellationToken)
+        {
+            ClientDataProcessorFactory = networkClientDataProcessorFactory;
+            CancellationToken = cancellationToken;
+
+            return await Task.FromResult(ClientThreadMeta.None);
+        }
+
+        /// <summary>
+        /// Invoke client connected event
+        /// </summary>
+        /// <param name="remoteEndPoint">The client remote end-point</param>
+        protected virtual void OnClientConnected(EndPoint? remoteEndPoint)
+        {
+            // Invoke client connect if possible
+            ClientConnected?.Invoke(this, new ClientConnectedEventArgs
+            {
+                RemoteEndPoint = remoteEndPoint,
+                Timestamp = DateTime.UtcNow
+            });
+        }
+
+        /// <summary>
+        /// Invoke client data received event
+        /// </summary>
+        /// <param name="remoteEndPoint">The client remote end-point</param>
+        /// <param name="data">Data received from client</param>
+        protected virtual void OnClientDataReceived(EndPoint? remoteEndPoint, object? data = null)
+        {
+            // Invoke client connect if possible
+            ClientDataReceived?.Invoke(this, new ClientDataReceivedEventArgs
+            {
+                Data = data,
+                RemoteEndPoint = remoteEndPoint,
+                Timestamp = DateTime.UtcNow
+            });
+        }
+
+        /// <summary>
+        /// Invoke client disconnected event
+        /// </summary>
+        /// <param name="remoteEndPoint">The client remote end-point</param>
+        /// <param name="ex">Optional exception that caused the disconnection</param>
+        protected virtual void OnClientDisconnected(EndPoint? remoteEndPoint, Exception? ex = null)
+        {
+            // Invoke client disconnect if possible
+            ClientDisconnected?.Invoke(this, new ClientDisconnectedEventArgs
+            {
+                Exception = ex,
+                RemoteEndPoint = remoteEndPoint,
+                Timestamp = DateTime.UtcNow
+            });
+        }
+
+        /// <summary>
+        /// Invoke client error event
+        /// </summary>
+        /// <param name="remoteEndPoint">The client remote end-point</param>
+        /// <param name="ex">Optional exception caught during client processing</param>
+        protected virtual void OnClientError(EndPoint? remoteEndPoint, Exception? ex = null)
+        {
+            // Invoke client error event
+            ClientError?.Invoke(this, new ClientErrorEventArgs
+            {
+                Exception = ex,
+                RemoteEndPoint = remoteEndPoint,
+                Timestamp = DateTime.UtcNow
+            });
+        }
+
+        public virtual void Dispose()
+        {
+        }
+    }
+}
